840709f27ff7df075787bb56a357011dad1c824d
Draco McDaniel <boobug2008@gmail.com>
2022-09-12 14:14:43 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,7 +6,7 @@ addLayer("p", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#EF0F0F",
+    color: "#EF1F3F",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
