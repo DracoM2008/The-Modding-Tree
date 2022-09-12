a14856d058d388cfdc58ce359e393fcb3fb6c6b9
Draco McDaniel <boobug2008@gmail.com>
2022-09-12 13:56:35 -0700

    Update mod.js
    The first version.

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The A Tree",
+	id: "0",
+	author: "DracoM2008",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
